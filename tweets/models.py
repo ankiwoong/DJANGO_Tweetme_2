2b7d2ea538dd980ae7eb5887846e7d52f2ba58e8
Ankiwoong <ankiwoong@gmail.com>
2020-06-29 07:43:05 +0900

    Append New Tweet & Reorder

--- tweets/models.py
+++ tweets/models.py
@@ -8,5 +8,8 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
+    class Meta:
+        ordering = ["-id"]
+
     def serialize(self):
         return {"id": self.id, "content": self.content, "likes": random.randint(0, 200)}
