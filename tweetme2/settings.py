81aebc7ca5e1c5f1e620928fa237a874f9740c65
Ankiwoong <ankiwoong@gmail.com>
2020-06-30 12:22:30 +0900

    Install Django Rest Framework

--- tweetme2/settings.py
+++ tweetme2/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third-parity
+    "rest_framework",
+    # internal
     "tweets",
 ]
 
