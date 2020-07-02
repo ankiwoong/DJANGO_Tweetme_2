a9c0564a7b2bd0a2f7235cfbf4b7417742315527
Ankiwoong <ankiwoong@gmail.com>
2020-07-02 12:23:52 +0900

    Adding a Like Field

--- tweets/models.py
+++ tweets/models.py
@@ -6,12 +6,21 @@ from django.conf import settings
 User = settings.AUTH_USER_MODEL
 
 
+class TweetLikes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class Tweet(models.Model):
     # Maps to SQL data
     # id = models.AutoField(primary_key=True)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE
     )  # many users can many tweets
+    likes = models.ManyToManyField(
+        User, related_name="tweet_user", blank=True, through=TweetLikes
+    )
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="images/", blank=True, null=True)
 
