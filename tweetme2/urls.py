17c1c726c87ea20b5ec1f78270dac27349ca687f
Ankiwoong <ankiwoong@gmail.com>
2020-07-06 17:58:28 +0900

    CSRF & Client Side Action Buttons

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -31,6 +31,6 @@ urlpatterns = [
     path("create-tweet/", tweet_create_view),
     path("tweets/", tweet_list_view),
     path("tweets/<int:tweet_id>", tweet_detail_view),
-    path("api/tweets/action/", tweet_action_view),
+    path("api/tweets/action", tweet_action_view),
     path("api/tweets/<int:tweet_id>/delete", tweet_delete_view),
 ]
