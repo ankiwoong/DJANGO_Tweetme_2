08ee247c0285e7cdf8743886c606fc52ab91ebd5
Ankiwoong <ankiwoong@gmail.com>
2020-07-15 09:57:24 +0900

    Handling a Form in React

--- tweetme2-web/src/App.js
+++ tweetme2-web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { TweetsList } from './tweets';
+import { TweetsComponent } from './tweets';
 
 function App() {
     return (
@@ -13,7 +13,7 @@ function App() {
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <div>
-                    <TweetsList />
+                    <TweetsComponent />
                 </div>
                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
