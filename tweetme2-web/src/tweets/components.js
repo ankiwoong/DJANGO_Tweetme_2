8970e7dab3e67d659a75bad076e815b5b37efb1d
Ankiwoong <ankiwoong@gmail.com>
2020-07-16 06:59:55 +0900

    Pass from Parent Component to Child with useEffect

--- tweetme2-web/src/tweets/components.js
+++ tweetme2-web/src/tweets/components.js
@@ -4,43 +4,57 @@ import { loadTweets } from '../lookup';
 
 export function TweetsComponent(props) {
     const textAreaRef = React.createRef();
+    const [newTweets, setNewTweets] = useState([]);
     const handleSubmit = (event) => {
         event.preventDefault();
         const newVal = textAreaRef.current.value;
-        console.log(newVal);
+        let tempNewTweets = [...newTweets];
+        // change this to a server side call
+        tempNewTweets.unshift({
+            content: newVal,
+            likes: 0,
+            id: 12313,
+        });
+        setNewTweets(tempNewTweets);
         textAreaRef.current.value = '';
     };
-
     return (
         <div className={props.className}>
             <div className="col-12 mb-3">
                 <form onSubmit={handleSubmit}>
-                    <textarea ref={textAreaRef} required={true} className="form-control"></textarea>
-                    <button type="submit" className="btn btn-primary my-3" name="tweet">
+                    <textarea ref={textAreaRef} required={true} className="form-control" name="tweet"></textarea>
+                    <button type="submit" className="btn btn-primary my-3">
                         Tweet
                     </button>
                 </form>
             </div>
-            <TweetsList />
+            <TweetsList newTweets={newTweets} />
         </div>
     );
 }
 
 export function TweetsList(props) {
+    const [tweetsInit, setTweetsInit] = useState([]);
     const [tweets, setTweets] = useState([]);
     useEffect(() => {
+        const final = [...props.newTweets].concat(tweetsInit);
+        if (final.length !== tweets.length) {
+            setTweets(final);
+        }
+    }, [props.newTweets, tweets, tweetsInit]);
+
+    useEffect(() => {
         const myCallback = (response, status) => {
-            console.log(response, status);
             if (status === 200) {
-                setTweets(response);
+                setTweetsInit(response);
             } else {
                 alert('There was an error');
             }
         };
         loadTweets(myCallback);
-    }, []);
+    }, [tweetsInit]);
     return tweets.map((item, index) => {
-        return <Tweet tweet={item} className="my-5 py-5 border bg-white text-dark" key={'${index}-{item.id}'} />;
+        return <Tweet tweet={item} className="my-5 py-5 border bg-white text-dark" key={`${index}-{item.id}`} />;
     });
 }
 
@@ -50,6 +64,7 @@ export function ActionBtn(props) {
     const [userLike, setUserLike] = useState(tweet.userLike === true ? true : false);
     const className = props.className ? props.className : 'btn btn-primary btn-sm';
     const actionDisplay = action.display ? action.display : 'Action';
+
     const handleClick = (event) => {
         event.preventDefault();
         if (action.type === 'like') {
@@ -82,7 +97,7 @@ export function Tweet(props) {
             <div className="btn btn-group">
                 <ActionBtn tweet={tweet} action={{ type: 'like', display: 'Likes' }} />
                 <ActionBtn tweet={tweet} action={{ type: 'unlike', display: 'Unlike' }} />
-                <ActionBtn tweet={tweet} action={{ type: 'retweet' }} />
+                <ActionBtn tweet={tweet} action={{ type: 'retweet', display: '' }} />
             </div>
         </div>
     );
