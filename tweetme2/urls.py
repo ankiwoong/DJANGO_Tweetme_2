22264c9963b9f34d8268025cce1d808511f38dc4
Ankiwoong <ankiwoong@gmail.com>
2020-07-31 07:17:18 +0900

    Clean Up API Urls and Views

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -16,23 +16,22 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path  # url
+from django.urls import path, re_path, include  # url()
 from django.views.generic import TemplateView
 
 from tweets.views import (
-    local_tweets_list_view,
-    local_tweets_detail_view,
-    local_tweets_profile_view,
+    tweets_list_view,
+    tweets_detail_view,
+    tweets_profile_view,
 )
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", local_tweets_list_view),
-    path("<int:tweet_id>", local_tweets_detail_view),
-    path("profile/<str:username>", local_tweets_profile_view),
-    path("api/tweets/", include("tweets.urls")),
+    path("", tweets_list_view),
+    path("<int:tweet_id>", tweets_detail_view),
+    path("profile/<str:username>", tweets_profile_view),
+    path("api/tweets/", include("tweets.api.urls")),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
