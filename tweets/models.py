6ebef911eff0d4c79a9ea7ddfdaddcf6ebfd422a
Ankiwoong <ankiwoong@gmail.com>
2020-08-06 15:24:01 +0900

    ManyToManyField and Reverse Relations

--- tweets/models.py
+++ tweets/models.py
@@ -16,7 +16,7 @@ class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
     parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE
+        User, on_delete=models.CASCADE, related_name="tweets"
     )  # many users can many tweets
     likes = models.ManyToManyField(
         User, related_name="tweet_user", blank=True, through=TweetLike
